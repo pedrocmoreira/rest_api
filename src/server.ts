4e5c148257615e85e105987866b093f456990b62
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-08-28 23:37:20 -0300

    chore: plugins do fastfy

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,11 @@
 import fastify from 'fastify';
-
-import { knex } from './database';
 import { env } from './env';
+
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*');
-
-  return transaction;
-});
+app.register(transactionsRoutes);
 
 app
   .listen({
