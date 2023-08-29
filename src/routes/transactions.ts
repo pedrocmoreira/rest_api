f2167bb724db5c325d7d959d89b910a069e3518b
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-08-28 23:49:46 -0300

    chore: criação de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,16 +1,26 @@
 import { FastifyInstance } from 'fastify';
 import { knex } from '../database';
+import { z } from 'zod';
+import { randomUUID } from 'crypto';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        amount: 1000,
-      })
-      .returning('*');
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    });
 
-    return transaction;
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    );
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
   });
 }
