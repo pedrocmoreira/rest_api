bc8eb8978996d8bdf87e576c2992f918a440ed25
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-09-25 18:15:10 -0300

    testando listagem de transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,8 @@
-import { test, beforeAll, afterAll, describe, it } from 'vitest';
+import { beforeAll, afterAll, describe, it, expect } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
+
+// jamais posso criar um teste que depende de outro teste
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -21,5 +23,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201);
+  });
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ]);
   });
 });
